e2d3015a98af060e04ec3abace8a463461e7004b
7utkarsh7 <utkarshdohare2@gmail.com>
2020-09-23 15:58:21 +0530

    first com

--- app.js
+++ app.js
@@ -126,6 +126,6 @@ app.get("/register",function(req,res){
 
 
 
-app.listen(3000 , function(){
-  console.log("Server started on port 3000.");
+app.listen(3000 || process.env.PORT , function(){
+  console.log("Server started on port.");
 });
